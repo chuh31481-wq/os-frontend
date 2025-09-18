f0f4ce99950592ddfe2cf912b5e5c6416e887f7a
chuh31481-wq <chuh31481@gmail.com>
2025-09-18 04:14:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         loginButton.addEventListener('click', () => {
             // Yeh Client ID aapke GitHub OAuth App ki hai.
             // Isay yahan likhne mein koi security risk nahi, yeh public hoti hai.
-            const GITHUB_CLIENT_ID = 'YAHAN_APNI_GITHUB_CLIENT_ID_PASTE_KAREIN';
+            const GITHUB_CLIENT_ID = 'Ov23liQlp764R1a7f51z';
             
             // User ko GitHub ke authorization page par bhejna
             window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=read:user`;
